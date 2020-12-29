a195c44e6b7669142f90ad261f7fea9c479a1f70
MKarimi <mkarimi21@hotmail.com>
2020-12-29 14:03:11 +0330

    description for project

---
+++ Splash_Screen_with_Python/Splash_Screen_PY/main.py
@@ -0,0 +1,9 @@
+###############################################################
+## 
+## PROJECT MADE WITH: QT Designer and PySide2
+##
+## By: Mostafa Karimi in Wanderson Courses
+## 
+## Version  0.1.1
+##
+###############################################################
